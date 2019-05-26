03757ccd442869e0b9e87043a6f319c7d4e508dd
xujintao <xujintao@126.com>
2019-05-26 23:16:52 +0800

    time bucket hash

--- types/map/main.go
+++ types/map/main.go
@@ -2,9 +2,10 @@ package main
 
 // 1, hashmap的桶数组
 // 2, kafka的partition数组
-// 3, golang的通道数组
-// 4, nginx的upstream数组
-// 5, mysql分库分表
+// 3, nginx的upstream数组
+// 4, mysql分库分表
+// 5, golang的通道数组
+// 6, golang的定时器桶数组
 // 对这些数组做负载均衡就是在做hash
 func main() {
 	m := map[string]int{
