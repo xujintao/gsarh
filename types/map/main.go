a614ae0fd2ed39b4a11f111c1f7b12a2bf74a509
xujintao <xujintao@126.com>
2019-06-03 04:38:41 +0800

    fd with multi process

--- types/map/main.go
+++ types/map/main.go
@@ -4,8 +4,9 @@ package main
 // 2, kafka的partition数组
 // 3, nginx的upstream数组
 // 4, mysql分库分表
-// 5, golang的通道数组
-// 6, golang的定时器桶数组
+// 5, tcp多进程监听器/udp多进程
+// 6, golang的通道数组
+// 7, golang的定时器桶数组
 // 对这些数组做负载均衡就是在做hash
 func main() {
 	m := map[string]int{
